7ed45d0fa881af8b7a7af85f3fc63da72563bc53
olaoyetijani <adeolatijani12@gmail.com>
2022-04-02 12:06:28 +0100

    e-commerce app

---
+++ src/components/cart/cartItem/CartItem.jsx
@@ -0,0 +1,28 @@
+import { Card, CardMedia, CardContent, Typography, CardActions, Button } from '@material-ui/core'
+import React from 'react'
+import useStyles from './styles'
+
+const CartItem = ({ item, handleUpdateCartQty, handleRemoveFromCart }) => {
+  const classes = useStyles()
+
+  return (
+    <Card>
+      <CardMedia image='item.image' className={classes.media}></CardMedia>
+      <CardContent className={classes.cardContent}>
+        <Typography variant='h4'>{item.name}</Typography>
+        <Typography variant='h5'>{item.line_total.formatted_with_symbol}</Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}>
+          <Button type='button' size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
+          <Typography>{item.quantity}</Typography>
+          <Button type='button' size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
+        </div>
+        <Button variant="contained" type="button" color="secondary" onClick={() => handleRemoveFromCart(item.id)}>Remove</Button>
+      </CardActions>
+      
+    </Card>
+  )
+}
+
+export default CartItem
